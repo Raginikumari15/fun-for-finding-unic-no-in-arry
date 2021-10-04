e952d0230cfe77a7cd5a16ba250af2b6b0b97f63
Ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-10-04 19:05:32 +0530

    Add files via upload

---
+++ main (51).cpp
@@ -0,0 +1,14 @@
+int findUnique(int *arr, int size)
+{
+    // find uniq no in array
+    for(int i=0;i<size;i++){
+        int j=0;
+        for(;j<size;j++){
+            if(i!=j && arr[i]==arr[j])
+                break;
+        }
+        
+        if(j==size)
+            return arr[i];
+    }
+}
